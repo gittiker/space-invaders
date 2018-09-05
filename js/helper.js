d60c802c129ee7bd3644132a7524c51f9dc11926
Marco <you@example.com>
2018-09-05 17:53:29 +0200

    Kill aliens

--- js/helper.js
+++ js/helper.js
@@ -1,6 +1,6 @@
 // Helper Functions
 function AABBIntersect(ax, ay, aw, ah, bx, by, bw, bh) {
-    return ax < bx+bw && by < ax+aw && ay < by+bh && by < ay+ah;
+    return ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah;
 };
 
 // Bullet
