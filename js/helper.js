be6cc147a77442172c1ba7c70c9b9b7a75a96d54
Marco <you@example.com>
2018-09-05 14:45:42 +0200

    Aliens Array

--- js/helper.js
+++ js/helper.js
@@ -14,7 +14,6 @@ function Display(width, height) {
 
 Display.prototype.drawSprite = function(sp, x, y) {
     // draw part of spritesheet to canvas
-    // this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
     this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
 };
 
